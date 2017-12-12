dfb97d6ce1a30fb78e8bbe957bb417eb6c96599f
Sébastien Rogier <srogier@decitre.fr>
2017-12-12 10:24:01 +0100

    validation asynchrone des commentaires

--- js/comment.js
+++ js/comment.js
@@ -10,4 +10,47 @@ $(document).ready(function(){
     });
 
 
+    commentForm.on('submit', function (event) {
+        var $form = $(this);
+        var commentFormMessages = $('.comment-form-messages', $form);
+
+        event.preventDefault();
+
+        var $submitBtn = $('#comment-form-submit', $form);
+        // Disable to prevent multiple submit
+        $submitBtn.attr('disabled', 'disabled');
+
+
+        var type = $form.attr('method'),
+            url = $form.attr('action'),
+            data = $form.serialize(),
+            contentType = 'application/x-www-form-urlencoded';
+
+        $submitBtn.data('prev-val', $submitBtn.val());
+        $submitBtn.val("Commentaire en cours d'envoi...");
+        $(commentFormMessages).html('');
+
+        $.ajax({
+            type: type,
+            url: url,
+            data: data,
+            contentType: contentType,
+            success: function (data) {
+                $form.find('textarea').val('');
+                commentFormMessages.append($('#post-new-comment-message-success', $form).html());
+            },
+            error: function (err) {
+                commentFormMessages.append($('#post-new-comment-message-error', $form).html());
+            },
+            complete: function () {
+                grecaptcha.reset();
+                $submitBtn
+                    .attr('disabled', null)
+                    .val($submitBtn.data('prev-val'))
+                ;
+            }
+        });
+    });
+
+
 });
